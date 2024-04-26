b5357605b8afa593ed372eee98ea9017aac67159
HB.C <869922457@qq.com>
2024-04-26 09:18:04 +0800

    translate：翻译页面搭建。

--- chatgpt_flutter/lib/pages/Translate_page.dart
+++ chatgpt_flutter/lib/pages/Translate_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatgpt_flutter/util/padding_extension.dart';
 import 'package:flutter/material.dart';
 
 class TranslatePage extends StatefulWidget {
@@ -8,8 +9,56 @@ class TranslatePage extends StatefulWidget {
 }
 
 class _TranslatePageState extends State<TranslatePage> {
+  TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          60.paddingHeight, // 使用SizedBox来创建间距
+          _languageSelectionArea(),
+          // 20.paddingHeight, // 使用SizedBox来创建间距
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 20.0),
+              itemCount: 2,
+              itemBuilder: (BuildContext context, int index) =>
+                  _translationArea(index),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 语种选择区域
+  _languageSelectionArea() {
+    return Container(
+      height: 60,
+      color: Colors.red,
+    );
+  }
+
+  // 翻译区域
+  _translationArea(int index) {
+    if (index == 0) {
+      return Padding(
+        padding: const EdgeInsets.only(right: 8, bottom: 20, left: 8),
+        child: TextField(
+          controller: _controller,
+          maxLines: null, // 或者设置为大于1的值，这样可以自动换行,
+          decoration: const InputDecoration(
+            labelText: 'Enter your text',
+            border: OutlineInputBorder(),
+          ),
+        ),
+      );
+    } else {
+      return const Padding(
+        padding: EdgeInsets.only(right: 8, left: 8),
+        child: Text('已经翻译'),
+      );
+    }
   }
 }
