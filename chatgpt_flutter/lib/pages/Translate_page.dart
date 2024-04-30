d4cd5a87e22158c8dfb2bed08bb4ede46905f6a0
HB.C <869922457@qq.com>
2024-04-30 18:49:37 +0800

    translate:翻译功能完善。

--- chatgpt_flutter/lib/pages/Translate_page.dart
+++ chatgpt_flutter/lib/pages/Translate_page.dart
@@ -1,7 +1,13 @@
+import 'package:chatgpt_flutter/dao/completion_dao.dart';
 import 'package:chatgpt_flutter/provider/theme_provider.dart';
+import 'package:chatgpt_flutter/util/hi_const.dart';
 import 'package:chatgpt_flutter/util/padding_extension.dart';
+import 'package:chatgpt_flutter/util/preferences_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_langdetect/flutter_langdetect.dart' as langdetect;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:openai_flutter/utils/ai_logger.dart';
 import 'package:provider/provider.dart';
 
 class TranslatePage extends StatefulWidget {
@@ -25,10 +31,36 @@ class _TranslatePageState extends State<TranslatePage>
   late List<String> _dropdownLeftValues;
   // 右下拉框数据
   late List<String> _dropdownRightValues;
+  get _themeProvider => context.watch<ThemeProvider>();
   get _themeColor => context.watch<ThemeProvider>().themeColor;
+  late CompletionDao completionDao;
+  // 是否等待翻译中
+  bool _isWaitTranslation = false;
+  // 已翻译文本
+  String? _translatedText;
+  // 语音播报对象
+  FlutterTts flutterTts = FlutterTts();
+  // 是否正在播报
+  bool _isBroadcast = false;
+  // 文心一言的accessToken
+  late String accessToken;
 
   @override
   bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+    _initWenXinConfig();
+  }
+
+  void _initWenXinConfig() async {
+    completionDao = CompletionDao();
+    accessToken = (await PreferencesHelper.loadData(HiConst.accessToken))!;
+    if (accessToken == '') {
+      accessToken = await CompletionDao.getWenXinToken();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +69,6 @@ class _TranslatePageState extends State<TranslatePage>
       AppLocalizations.of(context)!.automaticDetection,
       AppLocalizations.of(context)!.english,
       AppLocalizations.of(context)!.simplifiedChinese,
-      AppLocalizations.of(context)!.chineseTraditional,
       AppLocalizations.of(context)!.japanese,
       AppLocalizations.of(context)!.korean,
       AppLocalizations.of(context)!.spanishLanguage,
@@ -48,19 +79,18 @@ class _TranslatePageState extends State<TranslatePage>
     _dropdownRightValues = [
       AppLocalizations.of(context)!.english,
       AppLocalizations.of(context)!.simplifiedChinese,
-      AppLocalizations.of(context)!.chineseTraditional,
       AppLocalizations.of(context)!.japanese,
       AppLocalizations.of(context)!.korean,
       AppLocalizations.of(context)!.spanishLanguage,
       AppLocalizations.of(context)!.portuguese,
       AppLocalizations.of(context)!.french
     ];
+
     return Scaffold(
       body: GestureDetector(
         onTap: () {
           // 点击空白区域时取消焦点
           _focusNode.unfocus();
-          _onSend();
         },
         child: Column(
           children: [
@@ -81,31 +111,93 @@ class _TranslatePageState extends State<TranslatePage>
     );
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+    // 释放焦点节点
+    _focusNode.dispose();
+  }
+
+  ///发送给文心一言进行翻译
+  Future<String?> _onWenXinSend() async {
+    _selectedRightValue ??= _dropdownRightValues[0];
+    completionDao.resetChat();
+    String? response = '';
+    String inputMessage =
+        '请把冒号后面的内容翻译成$_selectedRightValue:${_controller.text}';
+    Locale locale = Localizations.localeOf(context);
+    String languageCode = locale.languageCode;
+    if (languageCode == 'en') {
+      // 本地语言是英文
+      inputMessage =
+          'Please translate the content after the colon into $_selectedRightValue:${_controller.text}';
+    }
+    try {
+      var map = await completionDao.createWenXinCompletions(
+          accessToken: accessToken,
+          prompt: inputMessage) as Map<String, dynamic>;
+      if (map['errorCode'] != null) {
+        if (map['errorCode'] == 110) {
+          PreferencesHelper.saveData(HiConst.accessToken, '');
+          _initWenXinConfig();
+          _onWenXinSend();
+          return null;
+        }
+      } else {
+        response = map['content'];
+        response = response?.replaceAll("**", "");
+        return response;
+      }
+    } catch (e) {
+      response = 'no response';
+      AILogger.log('TranslatePage:$response');
+      return null;
+    }
+  }
+
   // 语种选择区域
   _languageSelectionArea() {
     return Row(
       children: [
-        Flexible(
-            flex: 1,
-            child: _dropdownBtn(_dropdownLeftValues, _selectedLeftValue)),
+        Flexible(flex: 1, child: _dropdownBtn(_dropdownLeftValues, true)),
         Column(
           children: [
             Icon(Icons.arrow_back, color: Colors.grey[400]), // 向左指的图标
             Icon(Icons.arrow_forward, color: Colors.grey[400]), // 向右指的图标
           ],
         ),
-        Flexible(
-            flex: 1,
-            child: _dropdownBtn(_dropdownRightValues, _selectedRightValue))
+        Flexible(flex: 1, child: _dropdownBtn(_dropdownRightValues, false)),
+        Padding(
+          padding: const EdgeInsets.only(right: 8),
+          child: InkWell(
+            splashColor: Colors.transparent, // 水波纹颜色透明
+            highlightColor: Colors.transparent, // 高亮颜色透明
+            onTap: () {
+              _focusNode.unfocus();
+              _onTranslate();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: _themeColor, // 背景颜色
+                borderRadius: BorderRadius.circular(4.0), // 圆角
+              ),
+              padding: const EdgeInsets.all(4.0),
+              child: Text(AppLocalizations.of(context)!.translateForBtn,
+                  style: const TextStyle(
+                      color: Colors.white, fontSize: 16)), // InkWell的子部件
+            ),
+          ),
+        )
       ],
     );
   }
 
-  _dropdownBtn(List<String> dropdownValues, String? selectedValue) {
+  _dropdownBtn(List<String> dropdownValues, bool isLeft) {
     return Padding(
         padding: const EdgeInsets.only(right: 8, left: 8),
         child: DropdownButtonFormField<String>(
-          hint: Text(dropdownValues[0], style: const TextStyle(fontSize: 12)),
+          hint: Text(dropdownValues[0]),
           decoration: InputDecoration(
             contentPadding:
                 const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
@@ -119,13 +211,18 @@ class _TranslatePageState extends State<TranslatePage>
           ),
           onChanged: (newValue) {
             setState(() {
-              selectedValue = newValue;
+              if (isLeft) {
+                _selectedLeftValue = newValue;
+              } else {
+                _selectedRightValue = newValue;
+              }
+              _onTranslate();
             });
           },
           items: dropdownValues.map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
-              child: Text(value, style: const TextStyle(fontSize: 12)),
+              child: Text(value),
             );
           }).toList(),
         ));
@@ -156,6 +253,18 @@ class _TranslatePageState extends State<TranslatePage>
                     //回车发送消息
                     onSubmitted: (value) {
                       _onSend();
+                      // _isBroadcast = false;
+                      // flutterTts.stop();
+                    },
+                    onChanged: (text) {
+                      AILogger.log('Text changed: $text');
+                      if (text == '') {
+                        setState(() {
+                          _translatedText = null;
+                          _isBroadcast = false;
+                          flutterTts.stop();
+                        });
+                      }
                     },
                     maxLines: null, // 或者设置为大于1的值，这样可以自动换行,
                     textAlignVertical: TextAlignVertical.top, // 确保文字从上方开始
@@ -180,22 +289,63 @@ class _TranslatePageState extends State<TranslatePage>
                     top: 8,
                     right: 0,
                     child: IconButton(
-                      icon: Icon(
-                        Icons.clear,
-                        color: _themeColor,
-                      ),
+                      icon: Icon(Icons.clear, color: _themeColor),
                       onPressed: () {
-                        _controller.clear();
+                        setState(() {
+                          _controller.clear();
+                          _translatedText = null;
+                          _isBroadcast = false;
+                          flutterTts.stop();
+                        });
                       },
                     ),
                   )
                 ],
               )));
     } else {
-      return const Padding(
-        padding: EdgeInsets.only(right: 8, left: 8),
-        child: Text('已经翻译'),
-      );
+      if (_controller.text != '') {
+        return Padding(
+          padding: const EdgeInsets.only(right: 8, left: 8),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[350],
+              borderRadius: BorderRadius.circular(4), // 设置圆角半径为10
+            ),
+            padding: const EdgeInsets.fromLTRB(6, 6, 6, 0),
+            child: Column(
+              children: [
+                Text(
+                  _isWaitTranslation
+                      ? AppLocalizations.of(context)!.waitTranslation
+                      : _translatedText ?? '',
+                  style: TextStyle(color: _themeColor, fontSize: 16),
+                  textAlign: TextAlign.left,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: _themeProvider.screenSize!.width - 66,
+                    ),
+                    Expanded(
+                        child: IconButton(
+                      icon: Icon(
+                        Icons.volume_up,
+                        color:
+                            _isBroadcast == true ? Colors.black : _themeColor,
+                        size: 16,
+                      ),
+                      onPressed:
+                          !_isBroadcast ? _broadcastTranslatedText : null,
+                    ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Container();
+      }
     }
   }
 
@@ -205,6 +355,52 @@ class _TranslatePageState extends State<TranslatePage>
     _onTranslate();
   }
 
-  // TODO:去翻译
-  void _onTranslate() {}
+  // 翻译
+  void _onTranslate() async {
+    if (_controller.text != '') {
+      setState(() {
+        _isWaitTranslation = true;
+        _isBroadcast = true;
+      });
+      _translatedText = await _onWenXinSend();
+      setState(() {
+        _isWaitTranslation = false;
+        _isBroadcast = false;
+      });
+    }
+  }
+
+  // 播报已翻译文本
+  _broadcastTranslatedText() {
+    /// 语音播报开始
+    flutterTts.startHandler = () {
+      setState(() {});
+    };
+
+    /// 语音播报完成
+    flutterTts.completionHandler = () {
+      setState(() {
+        _isBroadcast = false;
+      });
+    };
+
+    /// 语音播报报错
+    flutterTts.errorHandler = (dynamic message) {
+      setState(() {
+        _isBroadcast = false;
+      });
+    };
+
+    _listenMessage();
+  }
+
+  //语音播报
+  Future _listenMessage() async {
+    _isBroadcast = true;
+    await langdetect.initLangDetect();
+    final language = langdetect.detect(_translatedText ?? '');
+    await flutterTts.setLanguage(language); // 设置语言，可以根据需要更改
+    await flutterTts.setPitch(1.0); // 设置音调，1.0为正常音调
+    await flutterTts.speak(_translatedText ?? '');
+  }
 }
